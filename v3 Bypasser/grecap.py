3fd65aaf72627b08544c4a32a764dfaa40b174de
x404xx <114883816+x404xx@users.noreply.github.com>
2023-05-23 05:54:08 +0800

    Update grecap.py

--- v3 Bypasser/grecap.py
+++ v3 Bypasser/grecap.py
@@ -1,4 +1,4 @@
-from re import findall
+import re
 from httpx import Client
 from user_agent import generate_user_agent as gua
 
@@ -11,17 +11,17 @@ def googRecap(proxy:str, aURL:str):
 
         #! Rebuild URL
         bURL = 'https://www.google.com/recaptcha/'
-        bURL += (matches := findall(r'([api2|enterprise]+)\/anchor\?(.*)', aURL)[0])[0]+'/'
+        bURL += (matches := re.findall(r'([api2|enterprise]+)\/anchor\?(.*)', url)[0])[0]
 
         #! Requests Token from Anchor URL
-        resp = session.get(f'{bURL}anchor', params=(param := matches[1]), timeout=10)
+        resp = session.get(f'{bURL}/anchor',headers=headers, params=(param := matches[1]), timeout=10)
 
         #! Convert Params To Dict
         sParams = dict(pair.split('=') for pair in param.split('&'))
 
         #! Update Data
-        payload = f"v={sParams['v']}&reason=q&c={findall(r'value=.(.*?).>', resp.text)[0]}&k={sParams['k']}&co={sParams['co']}"
+        payload = f"v={sParams['v']}&reason=q&c={re.search(r'value=.(.*?).>', resp.text)[1]}&k={sParams['k']}&co={sParams['co']}"
 
         #! Get Response Token
-        resp = session.post(f'{bURL}reload', params=f'k={sParams["k"]}', data=payload, timeout=10)
-        return(findall(r'"rresp","(.*?)"', resp.text)[0])
+        resp = session.post(f'{bURL}/reload',headers=headers, params=f'k={sParams["k"]}', data=payload, timeout=10)
+        return(re.search(r'"rresp","(.*?)"', resp.text)[1])
